d0c85e7acc52cd4ab18f4899ac5113522164fbe2
dev-juanda01 <penalozahernandezjuandavid03@gmail.com>
2025-03-05 19:44:29 -0500

    creation api client implementation

---
+++ src/adapters/api_client_impl.py
@@ -0,0 +1,22 @@
+import requests
+from typing import List
+from src.core.entities.data_record import DataRecord
+from src.ports.api_client import ApiClient
+
+class ApiClientImpl(ApiClient):
+    def fetch_data(self) -> List[DataRecord]:
+        response = requests.get('https://api.escuelajs.co/api/v1/products')
+        response.raise_for_status()
+        json_data = response.json()
+        data = [
+            DataRecord(
+                d['id'],
+                d['title'],
+                d['price'],
+                d['description'],
+                d['category'],
+                d['images'],
+            )
+            for d in json_data
+        ]
+        return data
