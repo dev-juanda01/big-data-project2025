6c9f5ac7839d7511468c1cf6511e49fc9b7b16c5
dev-juanda01 <penalozahernandezjuandavid03@gmail.com>
2025-03-05 19:38:31 -0500

    definition database base abstract

---
+++ src/ports/database.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+from typing import List
+from src.core.entities.data_record import DataRecord
+
+class Database(ABC):
+    @abstractmethod
+    def save_data(self, data: List[DataRecord]):
+        pass
+
+    @abstractmethod
+    def get_data(self) -> List[DataRecord]:
+        pass
